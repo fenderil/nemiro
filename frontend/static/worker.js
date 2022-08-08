c057c8cc683e71ea7bb381faf9c496587771cff4
tupitskiy ilya <fenderil@yandex.ru>
2022-08-08 04:12:47 +0300

    offline page

--- frontend/static/worker.js
+++ frontend/static/worker.js
@@ -1,18 +1,41 @@
 
-const cacheName = 'v1'
+const CACHE_NAME = 'v1'
 
 const contentToCache = [
-    '/favicon.ico'
+    '/static/nemiro.png'
 ]
+
+const OFFLINE_URL = '/static/offline.html'
     
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] Install')
     event.waitUntil((async () => {
-        const cache = await caches.open(cacheName)
+        const cache = await caches.open(CACHE_NAME)
         console.log('[Service Worker] Caching all: app shell and content')
 
         await cache.addAll(contentToCache)
+        await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }))
     })())
 })
 
-
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResponse = await event.preloadResponse
+                if (preloadResponse) {
+                    return preloadResponse
+                }
+        
+                const networkResponse = await fetch(event.request)
+                return networkResponse
+            } catch (error) {
+                console.log('Fetch failed; returning offline page instead.', error)
+        
+                const cache = await caches.open(CACHE_NAME)
+                const cachedResponse = await cache.match(OFFLINE_URL)
+                return cachedResponse
+            }
+        })())
+    }
+})
