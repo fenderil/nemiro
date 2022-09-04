74408295cabbe30897e017a51c691af664e137f9
tupitskiy ilya <fenderil@yandex.ru>
2022-09-04 13:46:22 +0300

    redesign logo

--- resize.js
+++ resize.js
@@ -14,11 +14,11 @@ const sizes = [
 const fs = require('fs')
 const path = require('path')
 
-const icon = fs.readFileSync(path.resolve(process.cwd(), 'frontend', 'static', 'nemiro-logo-m.svg'), 'utf8')
+const icon = fs.readFileSync(path.resolve(process.cwd(), 'frontend', 'static', 'nemiro-logo-n.svg'), 'utf8')
 
 sizes.forEach((size) => {
     fs.writeFileSync(
-        path.resolve(process.cwd(), 'frontend', 'static', `nemiro-logo-m-${size}x${size}.svg`),
+        path.resolve(process.cwd(), 'frontend', 'static', `nemiro-logo-n-${size}x${size}.svg`),
         icon
             .replace('height="144px" width="144px"', `height="${size}px" width="${size}px"`),
         'utf8',
