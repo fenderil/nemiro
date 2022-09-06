5a4b2cb92fd044ccce5e639602a891e776fbef5d
tupitskiy ilya <fenderil@yandex.ru>
2022-09-06 09:14:39 +0300

    easter egg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -94,3 +94,13 @@ nodes.canvasRoot.addEventListener('touchmove', scaleTouchMove)
 nodes.timerBtn.addEventListener('click', () => {
     toggleTimerState(true)
 })
+
+setInterval(() => {
+    document.querySelector('.rat').classList.add('active')
+    document.querySelector('.snake').classList.add('active')
+
+    setTimeout(() => {
+        document.querySelector('.rat').classList.remove('active')
+        document.querySelector('.snake').classList.remove('active')
+    }, 9000)
+}, 1000 * 60 * 10)
